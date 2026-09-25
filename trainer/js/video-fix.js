c02498068f7e547ee783068003ce6a3365efa1e6
Wrap911 <wrap911@users.noreply.github.com>
2026-09-25 17:08:22 +0000

    Make the video list script valid

--- trainer/js/video-fix.js
+++ trainer/js/video-fix.js
@@ -150,10 +150,10 @@
 
   function esc(s) {
     return String(s || "")
-      .replace(/&/g, "&")
-      .replace(/</g, "<")
-      .replace(/>/g, ">")
-      .replace(/"/g, """);
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;");
   }
 
   function renderVideos() {
